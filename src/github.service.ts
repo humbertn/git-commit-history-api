e98c4240426b6544500003ed51068a5ecf844d56
Gerson Estrada <gersonestrada.giron@gmail.com>
2023-09-15 13:28:08 -0600

    Adding dotenv configuration to add the auth token for the github api

--- src/github.service.ts
+++ src/github.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GithubService {
   private readonly githubAPIBaseURL = 'https://api.github.com';
+
+  constructor(private readonly configService: ConfigService) {} 
 
   async getCommitHistory(owner: string, repo: string) {
     if (owner == null || repo == null || owner.trim().length == 0 || repo.trim().length == 0) {
@@ -11,7 +14,12 @@ export class GithubService {
     }
 
     try {
-      const response = await axios.get(`${this.githubAPIBaseURL}/repos/${owner}/${repo}/commits`);
+      const accessToken = this.configService.get<string>('GITHUB_ACCESS_TOKEN');
+      const headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+
+      const response = await axios.get(`${this.githubAPIBaseURL}/repos/${owner}/${repo}/commits`, { headers });
       return response.data;
     } catch(error) {
       throw new Error(`Error while fetching commit history for owner:"${owner}" and repo:"${repo}". message:"${error.message}"`);
